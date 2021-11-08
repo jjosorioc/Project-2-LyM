1309250bad7e3382832205d00f813b8a92b31e1a
Juan José Osorio <j.osorioc@uniandes.edu.co>
2021-11-08 00:41:44 -0500

    Avances en el proyecto
    @larubiano0  para la output function (h) haz los métodos de retornar el siguiente en mayúscula o el actual en mayúscula

--- CodeDeCode/src-gen/CD3_2.java
+++ CodeDeCode/src-gen/CD3_2.java
@@ -63,9 +63,9 @@ public final class CD3_2 {
       $line=31;
       B=$fix($opIntvlY('0','9'));
       $line=32;
-      Object coder=$fix($invokeMethod(CD3_2.class,"createCoder",true,null,new Object[]{}));
+      Object coder=$fix(CD3_2.createCoder());
       $line=33;
-      Object deCoder=$fix($invokeMethod(CD3_2.class,"createDecoder",true,null,new Object[]{}));
+      Object deCoder=$fix(CD3_2.createDecoder());
       $line=40;
       Utilities.testCodeDecode($cast(gold.structures.automaton.ITransducer.class,coder),$cast(gold.structures.automaton.ITransducer.class,deCoder));
     }
